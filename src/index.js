9a8ed50a59d35712954a1f21c053f398d6ab3b94
Daniel <delmontejohndaniel1415@gmail.com>
2023-10-12 14:35:13 +0800

    allowing imports for images in a javascript file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import generateJoke from './generate-joke';
-
 import './styles/main.scss';
+import laughing from './assets/laughing.svg';
 
-console.log(generateJoke());
+const laughImg = document.getElementById('laughImg');
+const jokeBtn = document.getElementById('jokeBtn');
+
+laughImg.setAttribute('src', laughing);
+laughImg.style.width = '200px';
+
+window.addEventListener('load', generateJoke);
+
+jokeBtn.addEventListener('click', generateJoke);
